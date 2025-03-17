5e0a5630f38124a4aae902b4d07ec95dfcd368ec
Paddy <user@Mac.lan>
2025-03-17 22:07:38 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "flowbite-react";
+
+
+function App() {
+  return (
+      <div>
+          <Button>Click me</Button>
+      </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const flowbite = require("flowbite-react/tailwind");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./index.html", "./src/**/*.{js,ts,jsx,tsx}",
+        flowbite.content(),
+    ],
+    plugins: [
+        // ...
+        flowbite.plugin(),
+    ],
+};
