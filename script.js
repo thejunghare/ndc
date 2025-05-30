d06a79307a0e82e1d934ebe2144d16a6afc517c6
Prasad Junghare <86673407+thejunghare@users.noreply.github.com>
2025-05-30 08:55:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+//classs
+class User {
+  constructor(name) {
+    this.name = name;
+  }
+
+  //get name(){ return this._name}
+  // set name(vlaue){this._name = value}
+
+  sayHi() {
+    console.log(this.name);
+  }
+}
+
+// Inhertiance
+class Prasad extends User {}
+
+let prasad = new Prasad("arohi");
+prasad.sayHi();
+
+let user = new User("Prasad");
+user.sayHi(); // Prasad
+
+console.log(typeof User); // function
+
+// classes using function
+function classAsFunction(name) {
+  this.name = name;
+
+  classAsFunction.prototype.sayHello = function () {
+    console.log(this.name);
+  };
+}
+
+let userone = new classAsFunction("paddy");
+userone.sayHello();
+
+//promise
+let promise = new Promise(function (resolve, reject) {
+  //  resolve("done");
+
+  reject(new Error("error")); // ignore
+});
+
+promise
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error.message));
+
+new Promise((resolve, reject) => {
+  throw new Error("error fu");
+}).catch(console.log(error.message));
+
+// async/ await
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function fetchData() {
+  console.log("Fetching data...");
+
+  await delay(2000); // waits for 2 seconds
+
+  console.log("Data received!");
+}
+
+fetchData();
+
+async function fetchUser() {
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/users/1",
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const user = await response.json();
+    console.log("User Name:", user.name);
+    console.log("Email:", user.email);
+  } catch (error) {
+    console.error("Failed to fetch user:", error.message);
+  }
+}
+
+fetchUser();
